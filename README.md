880c9e9a542c9e0c4d362b8d1598f347fcc13149
ShookLyngs <shooklyngs@gmail.com>
2023-08-23 11:02:31 +0800

    chore: add more comments

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This is a simple example for beginners to try creating a spore on Nervos CKB.
 From the example, you should learn how to:
 
 1. Construct a transaction to create a spore on-chain with the [spore-sdk](https://github.com/sporeprotocol/spore-sdk)
-2. Sign the transaction with a [Secp256k1Blake160 Sign-all](https://github.com/nervosnetwork/ckb-system-scripts/blob/master/c/secp256k1_blake160_sighash_all.c) Wallet
+2. Sign the transaction with a [Secp256k1Blake160 Sign-all](https://github.com/nervosnetwork/ckb-system-scripts/blob/master/c/secp256k1_blake160_sighash_all.c) lock Wallet
 
 ## Run
 
--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,15 @@ import { createSecp256k1Wallet, fetchLocalImage } from './helpers';
   // 1. Select a predefined SporeConfig
   const config = predefinedSporeConfigs.Aggron4;
 
-  // 2. Create a Wallet from privateKey for signing transaction
+  // 2. Create a Secp256k1Blake160 Sign-all lock Wallet from privateKey for signing transaction,
+  // To understand the lock: https://github.com/nervosnetwork/ckb-system-scripts/blob/master/c/secp256k1_blake160_sighash_all.c
   const wallet = createSecp256k1Wallet('0xc153ee57dc8ae3dac3495c828d6f8c3fef6b1d0c74fc31101c064137b3269d6d', config);
 
   // 3. Fetch the `./image.jpg` image file from local, as the content of the new spore
   const content = await fetchLocalImage('./image.jpg');
 
-  // 4. The new spore's contentType should be paired with its content based on MIME types
+  // 4. The new spore's contentType should be paired with its content based on MIME types,
+  // refer to https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
   const contentType = 'image/jpeg';
 
   // 5. Construct a transaction to create a spore on-chain
